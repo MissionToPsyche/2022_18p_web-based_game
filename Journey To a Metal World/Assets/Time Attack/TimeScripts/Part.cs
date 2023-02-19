f992a7ea95dd994b4d1f33e7dee04a7fc34f6fd1
jonac19 <jonac19@uci.edu>
2023-02-19 13:02:26 -0800

    Parts generate a random ID

--- Journey To a Metal World/Assets/Time Attack/TimeScripts/Part.cs
+++ Journey To a Metal World/Assets/Time Attack/TimeScripts/Part.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class Part : MonoBehaviour
 {
-    private int END_X = -10;
+    private int id;
+    private static int END_X = -10;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        id = Random.Range(0, 6);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < END_X) {
+        if (transform.position.x < END_X) 
+        {
             Destroy(gameObject);
         }
+    }
+
+    public int getPartID()
+    {
+        return id;
     }
 }
--- Journey To a Metal World/Assets/Time Attack/TimeScripts/PartSpawner.cs
+++ Journey To a Metal World/Assets/Time Attack/TimeScripts/PartSpawner.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PartSpawner : MonoBehaviour
 {
     public GameObject part;
-    public float spawnRate = 3;
+    public float spawnRate = 2;
     private float timer = 0;
 
     // Start is called before the first frame update

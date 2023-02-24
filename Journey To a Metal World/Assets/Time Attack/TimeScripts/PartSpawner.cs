eebfc868bb5a2bf9378ab35e6840363048996cfb
jonac19 <jonac19@uci.edu>
2023-02-24 14:12:06 -0800

    Fixed floating point error

--- Journey To a Metal World/Assets/Time Attack/TimeScripts/PartSpawner.cs
+++ Journey To a Metal World/Assets/Time Attack/TimeScripts/PartSpawner.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PartSpawner : MonoBehaviour
 {
     public GameObject part;
-    public float spawnRate = 0.5;
+    public float spawnRate = 0.5f;
     private float timer = 0;
 
     // Start is called before the first frame update

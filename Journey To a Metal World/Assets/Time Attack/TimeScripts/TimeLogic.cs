903c48692b1238558e93a0d27dbc88b8dcbe2fc1
jonac19 <jonac19@uci.edu>
2023-02-26 19:33:58 -0800

    Can restart time attack minigame

--- Journey To a Metal World/Assets/Time Attack/TimeScripts/TimeLogic.cs
+++ Journey To a Metal World/Assets/Time Attack/TimeScripts/TimeLogic.cs
@@ -2,28 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TimeLogic : MonoBehaviour
 {
     [SerializeField] public Text timeText;
+    [SerializeField] public GameObject gameOver;
 
     private float timer;
+    private bool isGameOver;
 
     void Start() 
     {
-        timer = 30;
+        timer = 3;
+        isGameOver = false;
     }
 
     void Update() 
     {
-        if (timer > 0) {
-            timer -= Time.deltaTime;
-            updateTime();
+        if (!isGameOver) {
+            if (timer > 0) {
+                timer -= Time.deltaTime;
+                updateTime();
+            } else {
+                gameOver.SetActive(true);
+                isGameOver = true;
+            }
         }
+        
     }
 
     void updateTime()
     {
         timeText.text = "Time Left: " + ((int) timer).ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public bool getIsGameOver()
+    {
+        return isGameOver;
     }
 }

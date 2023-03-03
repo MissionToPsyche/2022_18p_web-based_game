3d96c992f3014dbc5410c1ce06984ba8f3e8d1e0
jonac19 <jonac19@uci.edu>
2023-03-03 11:39:56 -0800

    Basic functionality of carousel

---
+++ Journey To a Metal World/Assets/Main Menu/MainMenuScripts/ScreenCarousel.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenCarousel : MonoBehaviour
+{
+    [SerializeField] public GameObject scoreAttackScreen;
+    [SerializeField] public GameObject timeAttackScreen;
+    [SerializeField] public GameObject memoryScreen;
+    [SerializeField] public GameObject runnerScreen;
+
+    private SpriteRenderer rend;
+    private LinkedList<GameObject> carousel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        GameObject[] screens = {scoreAttackScreen, timeAttackScreen, memoryScreen, runnerScreen};
+        carousel = new LinkedList<GameObject>(screens);
+        
+        printCarousel();
+        
+    }
+
+    [ContextMenu("Next")]
+    void nextScreen() 
+    {
+        LinkedListNode<GameObject> oldScreen = carousel.First;
+        carousel.RemoveFirst();
+        carousel.AddLast(oldScreen);
+
+        printCarousel();
+    }
+
+    [ContextMenu("Previous")]
+    void prevScreen()
+    {
+        LinkedListNode<GameObject> newScreen = carousel.Last;
+        carousel.RemoveLast();
+        carousel.AddFirst(newScreen);
+
+        printCarousel();
+    }
+
+    void printCarousel()
+    {
+        string s = "[head, ";
+        foreach (GameObject o in carousel) {
+            s += o.name + ", ";
+        }
+        s += "tail]";
+        Debug.Log(s);
+    }
+}

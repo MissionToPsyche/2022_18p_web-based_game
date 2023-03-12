75da9dc668f3bc58396af3ad3f493947cf40e51e
jonac19 <jonac19@uci.edu>
2023-03-11 20:03:35 -0800

    Added phase C

--- Journey To a Metal World/Assets/About/Phase B/PhaseBScripts/PhaseBLogic.cs
+++ Journey To a Metal World/Assets/About/Phase B/PhaseBScripts/PhaseBLogic.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class PhaseBLogic : MonoBehaviour
 {
+    [ContextMenu("Next Phase")]
+    public void nextPhase()
+    {
+        SceneManager.LoadScene("Phase C");
+    }
+    
     [ContextMenu("Previous Phase")]
     public void prevPhase()
     {

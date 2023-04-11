c02571b0c40f101f96375b8f1033d62a56c96083
rycsue <Ryan.Sue104@gmail.com>
2023-04-10 17:41:39 -0700

    at this point there may be an unconfirmed point bug that I could not replicate. noting it down now

--- Journey To a Metal World/Assets/Runner/RunnerScripts/RunnerMeteoroidPoints.cs
+++ Journey To a Metal World/Assets/Runner/RunnerScripts/RunnerMeteoroidPoints.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// point system broken. something went wrong!
 public class RunnerMeteoroidPoints : MonoBehaviour
 {
     // Start is called before the first frame update

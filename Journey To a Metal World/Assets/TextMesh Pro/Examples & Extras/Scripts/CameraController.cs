1a5605672845064afad2be7b67f4ad10465a9d3d
jonac19 <jonac19@uci.edu>
2023-04-10 20:48:03 -0700

    Fixed NullReferenceExceptions

--- Journey To a Metal World/Assets/TextMesh Pro/Examples & Extras/Scripts/CameraController.cs
+++ Journey To a Metal World/Assets/TextMesh Pro/Examples & Extras/Scripts/CameraController.cs
@@ -198,7 +198,7 @@ namespace TMPro.Examples
                 // Check for left mouse button to select a new CameraTarget or to reset Follow position
                 if (Input.GetMouseButton(0))
                 {
-                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    UnityEngine.Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                     RaycastHit hit;
 
                     if (Physics.Raycast(ray, out hit, 300, 1 << 10 | 1 << 11 | 1 << 12 | 1 << 14))

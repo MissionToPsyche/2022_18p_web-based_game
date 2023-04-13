d080ac1985bcee123003286f1d8549279ad3e3b0
David Nguyen <davido.nguyen1212@gmail.com>
2023-04-12 19:22:44 -0700

    mini games animation on main menu screen

--- Journey To a Metal World/Assets/Main Menu/MainMenuScripts/GameCarousel.cs
+++ Journey To a Metal World/Assets/Main Menu/MainMenuScripts/GameCarousel.cs
@@ -9,9 +9,16 @@ public class GameCarousel : MonoBehaviour
     [SerializeField] public GameObject timeAttackGame;
     [SerializeField] public GameObject memoryGame;
     [SerializeField] public GameObject runnerGame;
+    [SerializeField] GameObject score_attack_animation;
+    [SerializeField] GameObject time_attack_animation;
+    [SerializeField] GameObject memory_animation;
+    [SerializeField] GameObject runner_animation;
 
     private SpriteRenderer rend;
     private LinkedList<GameObject> carousel;
+
+    // creates a mapping of the games array to the animated game objects
+    private Dictionary<GameObject, GameObject> animation_order = new Dictionary<GameObject, GameObject>();
 
 
     // Start is called before the first frame update
@@ -21,12 +28,22 @@ public class GameCarousel : MonoBehaviour
         GameObject[] games = {scoreAttackGame, timeAttackGame, memoryGame, runnerGame};
         carousel = new LinkedList<GameObject>(games);
 
-        GameObject game = carousel.First.Value;
-        rend.color = game.GetComponent<SpriteRenderer>().color;
+        // turning on animations
+        animation_order[games[0]] = score_attack_animation;
+        animation_order[games[1]] = time_attack_animation;
+        animation_order[games[2]] = memory_animation;
+        animation_order[games[3]] = runner_animation;
+
+        // used before animations were created
+        // GameObject game = carousel.First.Value;
+        // rend.color = game.GetComponent<SpriteRenderer>().color;
+
+        // enables the score attack animation
+        animation_order[games[0]].SetActive(true);
         
-        printCarousel();
-        
+        printCarousel();        
     }
+
 
     [ContextMenu("Next")]
     public void nextGame() 
@@ -34,23 +51,38 @@ public class GameCarousel : MonoBehaviour
         LinkedListNode<GameObject> oldGame = carousel.First;
         carousel.RemoveFirst();
         carousel.AddLast(oldGame);
+        
+        // disables the current game's animation
+        animation_order[oldGame.Value].SetActive(false);
 
         GameObject game = carousel.First.Value;
-        rend.color = game.GetComponent<SpriteRenderer>().color;
+
+        animation_order[game].SetActive(true);
+
+        // rend.color = game.GetComponent<SpriteRenderer>().color;
         printCarousel();
     }
+
 
     [ContextMenu("Previous")]
     public void prevGame()
     {
+        LinkedListNode<GameObject> oldGame = carousel.First;
+        // disables the current game's animation
+        animation_order[oldGame.Value].SetActive(false);
+
         LinkedListNode<GameObject> newGame = carousel.Last;
         carousel.RemoveLast();
         carousel.AddFirst(newGame);
 
         GameObject game = carousel.First.Value;
-        rend.color = game.GetComponent<SpriteRenderer>().color;
+
+        animation_order[game].SetActive(true);
+        
+        //rend.color = game.GetComponent<SpriteRenderer>().color;
         printCarousel();
     }
+
 
     public void playGame()
     {

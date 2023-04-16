4bcd1b21ea7b8c2df40567adb5d90e2a77070c14
jonac19 <jonac19@uci.edu>
2023-04-15 18:53:52 -0700

    Refactored assets and scripts

--- Journey To a Metal World/Assets/About/AboutScripts/OrbitalMoviement.cs
+++ Journey To a Metal World/Assets/About/AboutScripts/OrbitalMoviement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PhaseAOrbitalMovement : MonoBehaviour
+public class OrbitalMoviement : MonoBehaviour
 {
     [SerializeField] Transform start;
     [SerializeField] Transform middle;

26e016601ccdd62a61339f69136eac993f0a5091
ChingTingTsai <tct20422@gmail.com>
2023-05-03 10:56:29 -0700

    add ripple

--- Journey To a Metal World/Assets/Score Attack/ScoreScripts/AreaDetector.cs
+++ Journey To a Metal World/Assets/Score Attack/ScoreScripts/AreaDetector.cs
@@ -7,16 +7,25 @@ public class AreaDetector : MonoBehaviour
     private SpriteRenderer research_area;
     private float shrink_speed;
 
+    public GameObject ripple;
+    private Animator ripple_animator;
+    private SpriteRenderer ripple_sprite;
+
     void Start()
     {
         research_area = gameObject.GetComponent<SpriteRenderer>();
         shrink_speed = Random.Range(0.0001f, 0.000005f);
+        ripple = GameObject.Find("Ripple Animation");
+        ripple_animator = ripple.GetComponent<Animator>();
+        ripple_sprite = ripple.GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         if(StartScene.game_start == true){
             UpdatePosition();
+            UpdateRippleOrder();
+            
         }
     }
 
@@ -27,6 +36,16 @@ public class AreaDetector : MonoBehaviour
         if(ResearchAreaExist() && (research_area.transform.localScale.x >= 0.2f && research_area.transform.localScale.y >= 0.2f))
         {
             research_area.transform.localScale -= new Vector3(shrink_speed, shrink_speed, 0);
+        }
+    }
+
+    void UpdateRippleOrder()
+    {
+        if(ripple_animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1){
+            ripple_sprite.sortingOrder = 0;
+        }
+        else{
+            ripple_sprite.sortingOrder = 5;
         }
     }
 
@@ -41,6 +60,11 @@ public class AreaDetector : MonoBehaviour
         foreach (GameObject area in AreaGenerator.research_areas)
         {
             if(gameObject.transform.position == area.transform.position){
+                if(ripple){
+                    ripple_sprite.sortingOrder = 5;
+                    ripple_animator.transform.position = area.transform.position;
+                    ripple_animator.Play("Animation", 0, 0f);
+                }
                 AreaGenerator.research_areas.Remove(area);
                 break;
             }
